ece26988f3ed1713e9a764124597364e3c5d1fe0
leiwenpeng <leiwenpeng1994@outlook.com>
2020-11-30 15:36:58 +0800

    fix(disable breakline warn):

--- example/src/index.js
+++ example/src/index.js
@@ -1,16 +1,16 @@
-import { createElement, render, diff } from '@plain-react/virtual-dom';
+import {createElement, render, diff} from '@plain-react/virtual-dom';
 
 const node1 = createElement('div', {}, [
-  createElement('h1', { key: '1' }, ['Hello World']),
-  createElement('h2', { key: '2' }, ['Hello World']),
-  createElement('h3', { key: '3' }, ['Hello World']),
-  createElement('h4', ['Hello World']),
+  createElement('h1', {key: '1'}, ['Hello World']),
+  createElement('h2', {key: '2'}, ['Hello World']),
+  createElement('h3', {key: '3'}, ['Hello World']),
+  createElement('h4', ['Hello World'])
 ]);
 
 const node2 = createElement('div', [
-  createElement('h3', { key: '3' }, ['Hello World']),
-  createElement('h2', { key: '2' }, ['Hello World']),
-  createElement('h1', { key: '1' }, ['Hello World']),
+  createElement('h3', {key: '3'}, ['Hello World']),
+  createElement('h2', {key: '2'}, ['Hello World']),
+  createElement('h1', {key: '1'}, ['Hello World'])
 ]);
 
 render(node1);
