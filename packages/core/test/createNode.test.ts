dcbacd79acb2ac401d5b40d5b68c360da81d5cc4
leiwenpeng1994 <leiwenpeng1994@outlook.com>
2020-12-08 18:06:34 +0800

    修改测试用例

--- packages/core/test/createNode.test.ts
+++ packages/core/test/createNode.test.ts
@@ -1,11 +1,17 @@
-import {createTreeNode} from '@plain-react/core';
+import {createTreeNode, createTreeRoot} from '@plain-react/core';
 
 test('test create node', (done) => {
     const nodes = createTreeNode('div', 1, {}, [
         createTreeNode('span', 2, {}, []),
-        createTreeNode('span', 3, {}, [createTreeNode('span', 4, {}, [])])
+        createTreeNode('span', 3, {}, [createTreeNode('span', 4, {}, [])]),
+        createTreeNode('span', 5, {}, [])
     ]);
 
     console.log('nodes -->', nodes);
+
+    const root = createTreeRoot(nodes, document.getElementById('root') as Element);
+
+    console.log(root, root.rootNode, root?.rootNode?.siblings?.[0]);
+
     done();
 });
