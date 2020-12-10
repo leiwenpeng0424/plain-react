c7bc322a88fbcff242c53a50d15176a6c65ae458
leiwenpeng1994 <leiwenpeng1994@outlook.com>
2020-12-10 17:38:54 +0800

    fix(core): 更新一点代码, 格式化.

--- packages/core/test/createNode.test.ts
+++ packages/core/test/createNode.test.ts
@@ -1,8 +1,4 @@
-import {
-    createTreeNode,
-    createTreeRootNode,
-    findRootNode
-} from '@plain-react/core';
+import {createTreeNode, createTreeRootNode} from '@plain-react/core';
 
 test('test create node', (done) => {
     const nodes = createTreeNode('div', 1, {}, [
@@ -12,20 +8,10 @@ test('test create node', (done) => {
     ]);
 
     // console.log('nodes -->', nodes);
-    // const root = createTreeRoot(nodes, document.getElementById('root') as Element);
-    // console.log(root, root.rootNode, root?.rootNode?.next?.siblings?.[0]?.next);
-
-    done();
-});
-
-test('findRootNode', () => {
-    const a = createTreeNode('div', 1, {}, []);
-    const b = createTreeNode('div', 2, {}, []);
-    const c = createTreeNode('div', 3, {}, []);
-    const d = createTreeNode('div', 4, {}, [b]);
-    const nodes = createTreeNode('div', 5, {}, [a, b, c, d]);
 
     const root = createTreeRootNode(nodes);
-    console.log(root);
-    console.log(findRootNode(root.next));
+
+    console.log(root, root.rootNode, root.next);
+
+    done();
 });
